5fa0dfd28303b666b64fb56ea0d24b22b5bc7e2b
Per Minborg <minborg@speedment.com>
2020-05-14 12:03:10 +0200

    Add demo module

---
+++ demo/src/main/java/minborg/eventloop/demo/Main.java
@@ -0,0 +1,24 @@
+package minborg.eventloop.demo;
+
+import minborg.eventloop.EventLoop;
+import minborg.eventloop.component.EventLoopComponent;
+
+import java.util.NoSuchElementException;
+import java.util.ServiceLoader;
+
+public final class Main {
+
+    public static void main(String[] args) {
+
+        final EventLoopComponent eventLoopComponent = ServiceLoader.load(EventLoopComponent.class).stream()
+                .findFirst()
+                .map(ServiceLoader.Provider::get)
+                .orElseThrow(NoSuchElementException::new);
+
+        final EventLoop eventLoop = eventLoopComponent.create();
+
+        System.out.println(eventLoop);
+
+    }
+
+}
---
+++ demo/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module eventloop.demo {
+    uses minborg.eventloop.component.EventLoopComponent;
+
+    requires eventloop;
+    requires eventloop.provider.vanilla;
+}
